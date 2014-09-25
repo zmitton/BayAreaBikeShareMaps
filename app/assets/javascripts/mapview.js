d8ccf462838b2bc02834feb28bc67ad8d7ab1ffd
Theo Skolnik <theodoraskolnik@gmail.com>
2014-09-25 13:57:42 -0500

    made markerMaker

--- app/assets/javascripts/mapview.js
+++ app/assets/javascripts/mapview.js
@@ -1,34 +1,43 @@
-var map;
+// function initialize() {
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
 
-function initialize() {
-// if user input lat long, then latitude = input and longitude = input,
-// otherwise lat and long equal defaults
+
+
+// function initialize() {
+//   getlatlng();
+//   getMarker();
+//   renderMap();
+// }
+
+
+
+$(document).ready(function() {
+  var map;
   var zoom = 11
   var latitude = 41.8896848
   var longitude = -87.6377502
+  var latlng = new google.maps.LatLng(latitude, longitude);
 
-  var latlng = new google.maps.LatLng(latitude, longitude)
+  var getlatlng = function(lat, lng){
+    return new google.maps.LatLng(lat, lng);
+  };
 
   var mapOptions = {
     zoom: zoom,
     center: latlng
   };
+
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-
-  var marker = new google.maps.Marker({
-      position: latlng,
-      map: map,
-      title: 'Dev Bootcamp'
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-$(document).ready(function() {
+  var markerMaker = function(lat, lng, title) {
+    return new google.maps.Marker({
+          position: getlatlng(lat, lng),
+          map: map,
+          title: title
+      });
+  }
 
   $(".form").on("submit", function(event) {
     event.preventDefault();
@@ -41,8 +50,8 @@ $(document).ready(function() {
       e_latitude = response.end_location.lat
       e_longitude = response.end_location.lng
 
-    start_latlng = new google.maps.LatLng(s_latitude, s_longitude)
-    end_latlng = new google.maps.LatLng(e_latitude, e_longitude)
+      var marker1 = markerMaker(s_latitude, s_longitude, "Start");
+      var marker2 = markerMaker(e_latitude, e_longitude, "End");
 
     })
   });
