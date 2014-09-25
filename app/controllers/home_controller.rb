67f4dbfccbecbefa0bf90d4436c267659af1d65b
Theo Skolnik <theodoraskolnik@gmail.com>
2014-09-25 18:37:41 -0500

    divvy stations are on the map, booyaaah

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,18 @@ respond_to :json
     if @start_location && @end_location
       @start_location_coords = GoogleApi.get_coordinates_from_address(@start_location)
       @end_location_coords = GoogleApi.get_coordinates_from_address(@end_location)
-      coords_hash = {start_location: @start_location_coords, end_location: @end_location_coords}
+
+      @closest_start_station = Station.find_closest_to(@start_location_coords["lat"], @start_location_coords["lng"])
+
+      @closest_start_coords = Station.get_coords_of_station(@closest_start_station)
+
+
+      @closest_end_station = Station.find_closest_to(@end_location_coords["lat"], @end_location_coords["lng"])
+
+      @closest_end_coords = Station.get_coords_of_station(@closest_end_station)
+
+
+      coords_hash = {start_location: @start_location_coords, end_location: @end_location_coords, start_station: @closest_start_coords, end_station: @closest_end_coords}
       respond_with coords_hash
 
       # @direction_json = GoogleApi.get_directions_from_addresses(@start_location, @end_location)
