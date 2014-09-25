5bf2b714061d9d24817982e56c0621b9d5f71caa
Theo Skolnik <theodoraskolnik@gmail.com>
2014-09-25 10:27:11 -0500

    can parse lat and lng from start and end address

--- app/models/google_api.rb
+++ app/models/google_api.rb
@@ -1,7 +1,11 @@
+require 'json'
+
 class GoogleApi
   def self.get_coordinates_from_address(address)
   	uri = URI("http://maps.googleapis.com/maps/api/geocode/json?address=#{URI::escape(address)}")
-	json = Net::HTTP.get(uri)
+	 json = Net::HTTP.get(uri)
+   result = JSON.parse(json)
+   result["results"][0]["geometry"]["location"]
   end
 
   def self.get_directions_from_addresses(start_address, end_address, mode = "bicycling")
@@ -9,4 +13,5 @@ class GoogleApi
   	uri = URI("https://maps.googleapis.com/maps/api/directions/json?origin=#{URI::escape(start_address)}&destination=#{URI::escape(end_address)}&key=#{api}&avoid=highways&mode=#{mode}")
   	json = Net::HTTP.get(uri)
   end
+
 end
