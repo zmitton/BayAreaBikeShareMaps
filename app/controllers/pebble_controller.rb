105a6344149f158f74f75b51cdd14a8861fb4ab2
zac <zacmitton22@gmail.com>
2014-10-19 01:02:35 -0500

    models working

--- app/controllers/pebble_controller.rb
+++ app/controllers/pebble_controller.rb
@@ -1,6 +1,14 @@
 class PebbleController < ApplicationController
 respond_to :json
   def index
-    respond_with JSON.parse('{"BTC":{"USD":"395.536900000000","Ounces":0.32074026945852,"Grams":9.976137529929,"SilverOunces":22.692880095115,"SilverGrams":705.82746956358},"Gold":{"USD":"1199.200000000000","Ounces":3.1177874934045,"Grams":0.10023919555529},"Silver":{"USD":"17.430000000000","Ounces":0.044066685055174,"Grams":0.0014167768233285}}')
+    distances_from = Station.find_distances_from(params[:lat].to_f, params[:lng].to_f)
+    closest_station = Station.find(distances_from[0][0])
+
+    distance_in_miles = (distances_from[0][1] * 69.0).round(2)
+    direction = closest_station.cardinal_direction(params[:lat].to_f, params[:lng].to_f)
+    puts distance_in_miles
+    puts direction
+
+    respond_with closest_station
   end
 end
