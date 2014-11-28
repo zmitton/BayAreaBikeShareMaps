64c9aa807df38cb92dd46781d2a5bb48b3ac8ed2
peggyfox <fox.peggy@gmail.com>
2014-11-28 12:33:25 -0600

    fast forward back to holidivvy

--- app/controllers/holidivvy_controller.rb
+++
@@ -1,6 +0,0 @@
-class HolidivvyController < ApplicationController
-
-  def index
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/search', to: 'home#search', defaults: {format: :json}
+
   get '/stations', to: 'stations#index', defaults: {format: :json}
   get '/pebble', to: 'pebble#index', defaults: {format: :json}
-  get '/holidivvy', to: 'holidivvy#index'
   
 end
